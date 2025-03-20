d1a9085e4c1faff7ab6a0058dcc15a05fef1c075
Pascal Mielke <p.mielke87@gmx.de>
2025-03-20 21:02:37 +0100

    Add SVG icons for minus, bike, trash, and take-away; remove script.ts, refactoring

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-var _a;
-var menus = [
+const menus = [
     {
         id: 1,
         name: 'Pizza Margherita',
@@ -77,47 +76,236 @@ var menus = [
         img: 'assets/img/menus/greek-salad.jpg',
         favourite: false
     }
-];
-console.log(menus[0].name);
-var createMenuItemText = function (menu) {
-    return /*html */ "\n        <div class=\"menu-item-text\">\n            <h3>".concat(menu.name, "</h3>\n            <p>").concat(menu.description.join(', '), "</p>\n            <p class=\"menu-price\">").concat(menu.price, " \u20AC</p>\n        </div>\n    ");
+],
+      getCart = () => JSON.parse(localStorage.getItem('cart') || '[]'),
+      saveCart = (cart) => localStorage.setItem('cart', JSON.stringify(cart));
+
+const createMenuItemText = (menu) => {
+    return /*html */`
+        <div class="menu-item-text">
+            <h3>${menu.name}</h3>
+            <p>mit ${menu.description.join(', ')}</p>
+            <p class="menu-price">
+                ${menu.price.toLocaleString('de-DE', {
+                    style: 'currency',
+                    currency: 'EUR',
+                })}
+            </p>
+        </div>
+    `;
 };
-var createMenuItemImageSection = function (menu) {
-    return /*html */ "\n        <div class=\"menu-item-image-section\">\n            <img class=\"menu-item-image\" src=\"".concat(menu.img, "\" alt=\"").concat(menu.name, "\">\n            <img class=\"add-to-cart small-logo\" id=\"add-to-cart\" src=\"assets/img/plus.svg\" alt=\"Add to cart\" onclick=\"addToCart(").concat(menu.id, ")\">\n        </div>\n    ");
+
+const createMenuItemImageSection = (menu) => {
+    return /*html */`
+        <div class="menu-item-image-section">
+            <img class="menu-item-image" src="${menu.img}" alt="${menu.name}">
+            <img class="add-to-cart small-logo" id="add-to-cart" src="assets/img/plus.svg" alt="Add to cart">
+        </div>
+    `;
 };
-var articleTemplate = function (menu) {
-    var article = /*html */ "\n        <article class=\"menu-item\" data-index=\"".concat(menu.id, "\">\n            ").concat(createMenuItemText(menu), "\n            ").concat(createMenuItemImageSection(menu), "\n        </article>\n    ");
-    return article;
+
+const articleTemplate = (menu) => {
+    return /*html */`
+        <article class="menu-item" data-index="${menu.id}" onclick="openMenuPopup(${menu.id})">
+            ${createMenuItemText(menu)}
+            ${createMenuItemImageSection(menu)}
+        </article>
+    `;
 };
-window.addToCart = function (id) {
-    // console.log('Add to cart', id);
-    var cart = JSON.parse(localStorage.getItem('cart') || '[]');
-    cart.push(id);
-    localStorage.setItem('cart', JSON.stringify(cart));
-};
-menus.forEach(function (menu) {
-    var menuFavorites = document.getElementById('menu-items-favorites');
-    var menuOffers = document.getElementById('menu-items-offers');
+
+const openMenuPopup = (id) => {
+    let menuPopup = document.getElementById('menu-popup'),
+        menuPopupDescriptionText = document.getElementById('menu-popup-description');
+    if (!menuPopup) {
+        throw new Error('Element not found');
+    }
+    menuPopupDescriptionText.innerHTML = /*html */`
+        <div class="menu-popup-header">
+            <img class="menu-popup-close" id="menu-popup-close" src="assets/img/xmark.svg" alt="Close" onclick="closePopup()">
+            <img class="menu-popup-image" src="${menus[id - 1].img}" alt="Pizza Margherita">
+        </div>
+        <div class="menu-popup-description-text" id="menu-popup-description-text">
+            <h3>${menus[id - 1].name}</h3>
+            <p class="menu-price">
+                ${menus[id - 1].price.toLocaleString('de-DE', {
+                    style: 'currency',
+                    currency: 'EUR',
+                })}
+            </p>
+            <p>mit ${menus[id - 1].description.join(', ')}</p>
+        </div>
+        <footer class="menu-popup-footer">
+            <div class="menu-popup-quantity">
+                <img class="decrease-quantity small-logo" src="assets/img/minus.svg" alt="decrease the quantity" onclick="decreaseMenuQuantity(${id - 1})">
+                <p class="quantity" id="quantity">1</p>
+                <img class="increase-quantity small-logo" src="assets/img/plus.svg" alt="increase the quantity" onclick="increaseMenuQuantity(${id - 1})">
+            </div>
+            <div class="total-price" id="total-price" onclick="addToCart(${id})">${menus[id - 1].price.toLocaleString('de-DE', {
+                    style: 'currency',
+                    currency: 'EUR',
+                })}
+            </div>
+        </footer>
+    `;
+
+    menuPopup.style.display = 'flex';
+}
+
+menus.forEach(menu => {
+    let menuFavorites = document.getElementById('menu-items-favorites'),
+        menuOffers = document.getElementById('menu-items-offers');
+
     if (!menuFavorites || !menuOffers) {
         throw new Error('Element not found');
     }
+
     if (menu.favourite) {
         menuFavorites.innerHTML += articleTemplate(menu);
-    }
-    else {
+    } else {
         menuOffers.innerHTML += articleTemplate(menu);
     }
 });
-var cartMenuButton = document.getElementById('cart-menu-button');
-var cartMenuSection = document.getElementById('cart-section');
+
+const decreaseMenuQuantity = (id) => {
+    let quantity = document.getElementById('quantity'),
+        totalPrice = document.getElementById('total-price'),
+        newQuantity = parseInt(quantity.innerText) - 1;
+
+    if (newQuantity < 1) {
+        return;
+    }
+
+    quantity.innerText = newQuantity;
+    totalPrice.innerText = (newQuantity * menus[id].price).toLocaleString('de-DE', {
+        style: 'currency',
+        currency: 'EUR',
+    });
+};
+
+const increaseMenuQuantity = (id) => {
+    let quantity = document.getElementById('quantity'),
+        totalPrice = document.getElementById('total-price'),
+        newQuantity = parseInt(quantity.innerText) + 1;
+
+    quantity.innerText = newQuantity;
+    totalPrice.innerText = (newQuantity * menus[id].price).toLocaleString('de-DE', {
+        style: 'currency',
+        currency: 'EUR',
+    });;
+}
+
+const decreaseCardItemQuantity = (id = null) => {
+    let cart = getCart(),
+        item = cart.find(item => item.id === id),
+        itemAmount = document.getElementById(`cart-item-amount-${id}`);
+
+    if (item) {
+        item.amount -= 1;
+        if (item.amount === 0) {
+            cart = cart.filter(item => item.id !== id);
+        }
+    }
+
+    cart = cart.filter(item => item.amount !== 0);
+    saveCart(cart);
+    loadCart();
+};
+
+const increaseCardItemQuantity = (id) => {
+    let cart = getCart(),
+        item = cart.find(item => item.id === id),
+        cartItemPrice = document.getElementById('cart-item-price'),
+        itemAmount = document.getElementById(`cart-item-amount-${id}`);
+
+    if (item) {
+        item.amount += 1;
+    } else {
+        cart.push({id: id, amount: 1});
+    }
+
+    saveCart(cart);
+    loadCart();
+};
+
+const addToCart = (id) => {
+    let cart = getCart(),
+    item = cart.find(item => item.id === id),
+    quantity = +document.getElementById('quantity')?.innerText,
+    itemAmount = document.getElementById(`cart-item-amount-${id}`);
+
+    if (item) {
+        item.amount += quantity;
+    }
+    else {
+        cart.push({id: id, amount: parseInt(quantity)});
+    }
+    saveCart(cart);
+    loadCart();
+    closePopup();
+};
+// need this???
+// const addToCart = (id) => {
+//     increaseCardItemQuantity(id);
+// };
+
+// window.addToCart = (id) => {
+//     // console.log('Add to cart', id);
+//     let cart = JSON.parse(localStorage.getItem('cart') || '[]');
+//     cart.push({id: id, amount: 1});
+//     localStorage.setItem('cart', JSON.stringify(cart));
+//     increaseCardItemQuantity(id);
+// };
+
+const cartMenuButton = document.getElementById('cart-menu-button'),
+    cartMenuSection = document.getElementById('cart-section');
+
 if (cartMenuButton && cartMenuSection) {
     cartMenuButton.onclick = function () {
-        cartMenuSection === null || cartMenuSection === void 0 ? void 0 : cartMenuSection.classList.toggle('show-header-menu');
+        cartMenuSection?.classList.toggle('show-header-menu');
         console.log('cartMenuButton', cartMenuButton);
     };
 }
-var closeCart = function () {
-    var _a;
-    (_a = document.getElementById('cart-section')) === null || _a === void 0 ? void 0 : _a.classList.remove('show-header-menu');
+
+const closeCart = () => {
+    document.getElementById('cart-section').style.display = 'none';
 };
-(_a = document.getElementById('cart-header-close')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', closeCart);
+
+document.getElementById('cart-header-close')?.addEventListener('click', closeCart);
+
+const loadCart = () => {
+    let cart = getCart(),
+        cartItems = document.getElementById('cart-items');
+
+    cartItems.innerHTML = '';
+
+    cartItems.innerHTML = cart.map(item => createCartItem(item.id, item.amount)).join('');
+};
+
+const createCartItem = (id, amount) => {
+    let menu = menus[id - 1];
+    return /*html */`
+        <article class="cart-item" data-index="${menu.id}">
+            <div class="cart-item-menu">
+                <p>${menu.name}</p>
+                <p class="">mit ${menu.description.join(', ')}</p>
+            </div>
+            <div class="cart-item-price-quantity">
+                <p id="cart-item-price-${menu.id}">
+                    ${(menu.price * amount).toLocaleString('de-DE', {
+                        style: 'currency',
+                        currency: 'EUR',
+                    })}
+                </p>
+                <div class="cart-item-quantity">
+                    <img class="substract-from-cart-item small-logo" src="assets/img/minus.svg" alt="Substract from cart" onclick="decreaseCardItemQuantity(${menu.id})">
+                    <p id="cart-item-amount-${menu.id}">${amount}</p>
+                    <img class="add-to-cart-item small-logo" src="assets/img/plus.svg" alt="Add to cart" onclick="increaseCardItemQuantity(${menu.id})">
+                </div>
+            </div>
+        </article>
+    `;
+}
+
+const closePopup = () => {
+    document.getElementById('menu-popup').style.display = 'none';
+};
